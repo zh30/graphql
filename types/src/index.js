08f9747ab2c590e31bf1b67dccd8c1e64f28e985
zhanghe <zhanghelook@outlook.com>
2019-01-10 20:10:52 +0800

    尝试学习携带参数

--- types/src/index.js
+++ types/src/index.js
@@ -1,12 +1,9 @@
-const {
-  ApolloServer,
-  gql
-} = require('apollo-server');
+const { ApolloServer, gql } = require("apollo-server");
 
-const typeDefs = gql `
+const typeDefs = gql`
   type Query {
-    hello: String!
-    user: User!
+    hello(name: String): String!
+    user: [User]!
   }
 
   type User {
@@ -38,43 +35,55 @@ const typeDefs = gql `
 
   type Mutation {
     books(title: String!, Author: String, id: ID): Books!
-    login(userinfo: UserInfo!): Boolean!
+    login(userinfo: UserInfo!): String!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'world',
-    user: () => ({
-      username: 'zhanghe',
-      id: '23413123'
-    })
+    hello: (parent, { name }, context, info) => {
+      return `你好，最最辛苦的${name}!`;
+    },
+    user: () => [
+      {
+        username: "zhanghe",
+        id: "23413123"
+      },
+      {
+        username: "zhanghe",
+        id: "23413133"
+      }
+    ]
   },
   Mutation: {
     books: () => ({
-      errors: [{
-          field: 'username',
-          message: 'ops!!!'
+      errors: [
+        {
+          field: "username",
+          message: "ops!!!"
         },
         {
-          field: 'username',
-          message: 'ops!!!'
-        },
+          field: "username",
+          message: "ops!!!"
+        }
       ],
       book: {
         id: 1,
-        title: 'yes!!!',
-        author: 'zhanghe'
+        title: "yes!!!",
+        author: "zhanghe"
       }
-    })
+    }),
+    login: (parent, { userinfo: { username } }, context, info) => {
+      console.log(context);
+      return username;
+    }
   }
-}
+};
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: ({ req, res }) => ({ req, res })
 });
 
-server.listen().then(({
-  url
-}) => console.log(`Graphql server at ${url}`))
+server.listen().then(({ url }) => console.log(`Graphql server at ${url}`));
