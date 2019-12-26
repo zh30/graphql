70b47a8f2d1fb90cc75989a1fe20d09ff5ce4311
ZhangHe <zhangheorg@outlook.com>
2019-12-26 17:48:36 +0800

    feat(index.js): 增加type Photo

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 const { ApolloServer } = require("apollo-server");
 
 const typeDefs = `
+    type Photo {
+        id: ID!
+        url: String!
+        name: String!
+        description: String!
+    }
     type Query {
         totalPhotos: Int!
+        allPhotos: [Photo!]
     }
     type Mutation {
-        postPhoto(name: String! description: String): Boolean!
+        postPhoto(name: String! description: String): Photo!
     }
 `;
+let _id = 0;
 let photos = [];
 const resolvers = {
   Query: {
-    totalPhotos: () => photos.length
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos
   },
   Mutation: {
     postPhoto(parent, args) {
-      photos.push(args);
-      return true;
+      const newPhoto = {
+        id: _id++,
+        ...args
+      };
+      photos.push(newPhoto);
+      return newPhoto;
     }
+  },
+  Photo: {
+    url: ({ id }) => `https://zhanghe.org/images/${id}.jpg`
   }
 };
 const server = new ApolloServer({
